67d79967799317f3cc32cb1695ad1d54b7050f50
nodekiaz <node.kiaz@gmail.com>
2026-04-05 21:50:01 +0200

    upload api

--- api.py
+++ api.py
@@ -52,3 +52,14 @@ def calculate(data: CalculationRequest):
 @app.get("/")
 def root():
     return {"status": "API running"}
+
+
+# -------------------------
+# 🚀 START SERVER
+# -------------------------
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("api:app", host="0.0.0.0", port=port)
